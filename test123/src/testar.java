00e816557255b00a78bf88023b843300fcef15cc
TTmesb <emil.aberg@elev.ga.ntig.se>
2020-10-16 10:50:16 +0200

    test

---
+++ test123/src/testar.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class testar {
+    public static void main(String[] arg) {
+
+        int knappNr = JOptionPane.showConfirmDialog(null, "Vill åka bort på en trevlig resa med din käraste");
+
+        String dagar = "Hur många dagar vill ni vara bortrest";
+        if (knappNr == 0) {
+            String antal = JOptionPane.showInputDialog(null, dagar);
+            int pris = 1150;
+            int antal1 = Integer.parseInt((antal));
+            JOptionPane.showMessageDialog(null, "Då blir eran totala kostnad " + pris * antal1);
+
+        } else if (knappNr == 1) {
+            String anledning = JOptionPane.showInputDialog(null, "Varför inte? :(");
+            JOptionPane.showMessageDialog(null, "Ditt svar = " + anledning + "\nOkej, vi ska tänka på det tills nästa gång");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Bokning avbruten");
+            System.exit(0);
+        }
+    }
+}
