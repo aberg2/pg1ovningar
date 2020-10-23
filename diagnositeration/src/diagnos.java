81e293de1039cbdc68c8eb8296f5394c14eef2b3
TTmesb <emil.aberg@elev.ga.ntig.se>
2020-10-23 11:21:53 +0200

    diagnosen

---
+++ diagnositeration/src/diagnos.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+/**
+ * Skapat oktober 2020-10-23 - 10:39
+ * diagnos
+ * Skriven av coolingen
+ */
+public class diagnos {
+    public static void main(String[] args) {
+        /*  int siffran = Integer.parseInt(JOptionPane.showInputDialog("Ange en siffra"));
+        int antal =0;
+        int svar = antal*siffran;
+        while (antal < 10) {
+            antal++;
+
+            System.out.println(antal+ "*"+ siffran + "=" + antal*siffran);
+        } */ // multiplikationstabell med while loop
+        /* int siffran = Integer.parseInt(JOptionPane.showInputDialog("Ange en siffra"));
+        int antal = 0;
+        int svar = antal * siffran;
+        for (int i = 0; antal <= 10; antal++) {
+            System.out.println(siffran + "*" + antal + "=" + siffran * antal);
+        } */ //multiplikationstabell med for loop
+    }
+}
