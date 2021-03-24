e1d8db23df8a2c22ad59d5d81b44a9bcec24937a
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-03-24 11:57:04 +0100

    walking2

--- walkingsimulator/src/walk.java
+++ walkingsimulator/src/walk.java
@@ -1,7 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 /**
  * Skapat mars 2021-03-23 - 11:14
  * walk
  * Skriven av coolingen
  */
-public class walk {
+public class walk extends Canvas implements Runnable{
+    private int width = 800;
+    private int height = 600;
+
+
+    public walk() {
+        JFrame frame = new JFrame("Walking Simulator");
+        this.setSize(width, height);
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    public void ritning(){
+        Graphics g = bs.getDrawGraphics();
+
+    }
+    @Override
+    public void run() {
+
+    }
 }
